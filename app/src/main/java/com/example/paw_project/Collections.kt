9622ef9e43110ae5db318713a95710671e60ead5
Kakonoomoide <rizkiramada128@gmail.com>
2024-10-11 17:05:32 +0700

    Add - SetExample function for demonstrating Set operations

--- app/src/main/java/com/example/paw_project/Collections.kt
+++ app/src/main/java/com/example/paw_project/Collections.kt
@@ -1,16 +1,16 @@
 package com.example.paw_project
 
-// list
+// A list is an ordered collection of elements that can contain duplicates
 fun ListExample(){
     println("=== LIST ===")
 
     // Read-Only list
-    // ..You can`t change value of list if list use listof
+    // .. You can`t change value of list if list use listof
     val readOnlyList = listOf("A","B","C")
     println(readOnlyList)
 
     // Mutable list
-    // ..You can change value of list if the list use muablelistof
+    // .. You can change value of list if the list use muablelistof
     val shape: MutableList<String> = mutableListOf("Circle","Square","Triangle")
     println(shape)
 
@@ -31,9 +31,32 @@ fun ListExample(){
     println(shapeLocked)
 }
 
-// set
+// A set is an unordered collection of unique elements.
 fun SetExample(){
+    println()
+    println("=== Set ===")
 
+    // Read-Only set
+    // .. A set created using `setOf()` contains unique elements and cannot be modified.
+    val readOnlySet = setOf("A","B","C")
+    println(readOnlySet)
+
+    // Mutable Set
+    // .. A set created using `mutableSetOf()` allows for dynamic modification.
+    val shape: MutableSet<String> = mutableSetOf("Circle","Square","Triangle")
+    println(shape)
+
+    // Add data to MutableSet (Shape)
+    shape.add("Rectangle")
+    println(shape)
+
+    // Remove data to MutableSet (Shape)
+    shape.remove("Circle")
+    println(shape)
+
+    // Change mutable list to read-only list
+    val shapeLocked: Set<String> = shape
+    println(shapeLocked)
 }
 
 // map
@@ -43,4 +66,5 @@ fun MapExample(){
 
 fun main(){
     ListExample()
+    SetExample()
 }
