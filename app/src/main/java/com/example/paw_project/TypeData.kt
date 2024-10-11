301bf820c06cc6237e4d932766f9962b0b626123
Kakonoomoide <rizkiramada128@gmail.com>
2024-10-11 16:37:02 +0700

    add - add usage example of typedata in kotlin

--- app/src/main/java/com/example/paw_project/TypeData.kt
+++ app/src/main/java/com/example/paw_project/TypeData.kt
@@ -1,2 +1,43 @@
 package com.example.paw_project
 
+fun main(){
+    //byte
+    val bytevalue: Byte = 10
+    println("Byte: $bytevalue")
+
+    //short
+    val shortvalue: Short = 20
+    println("Short: $shortvalue")
+
+    //int
+    val intvalue: Int = 30
+    println("Int: $intvalue")
+
+    //long
+    val longvalue: Long = 40
+    println("Long: $longvalue")
+
+    //float
+    val floatvalue: Float = 50.6f
+    println("Long: $floatvalue")
+
+    //double
+    val doublevalue: Double = 40.3
+    println("Long: $doublevalue")
+
+    //char
+    val charvalue: Char = 'R'
+    println(charvalue)
+
+    //bolean
+    val boleanvalue: Boolean = true
+    println("Bolean: $boleanvalue")
+
+    //string
+    var stringvalue: String = "Hello, world!"
+    println("String: $stringvalue")
+
+    //string
+    stringvalue = "Hello, kotlin!"
+    println("String: $stringvalue")
+}
