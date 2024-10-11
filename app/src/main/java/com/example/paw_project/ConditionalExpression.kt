4944744f546d2399e7e9340c98fdaf606b2d0361
Kakonoomoide <rizkiramada128@gmail.com>
2024-10-11 17:22:24 +0700

    Add - if-else statement to check if a number is positive or negative

--- app/src/main/java/com/example/paw_project/ConditionalExpression.kt
+++ app/src/main/java/com/example/paw_project/ConditionalExpression.kt
@@ -1,2 +1,23 @@
 package com.example.paw_project
 
+fun ConditionalStatement(){
+    println("==== Conditional Expression ====")
+
+    // If-Else Statement
+    // - Checks if the variable `number` is greater than 0.
+    // - If true, prints "Positive Number"; otherwise, prints "Negative Number".
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
+}
+
+fun looping(){
+
+}
+
+fun main(){
+
+}
