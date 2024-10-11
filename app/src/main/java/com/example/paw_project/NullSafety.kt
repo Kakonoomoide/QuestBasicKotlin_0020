b5ffe09f08fd9d76d2fe74182cfb2b202b8473a8
Kakonoomoide <rizkiramada128@gmail.com>
2024-10-11 18:54:49 +0700

    Add - variable declarations to demonstrate null safety

--- app/src/main/java/com/example/paw_project/NullSafety.kt
+++ app/src/main/java/com/example/paw_project/NullSafety.kt
@@ -1,5 +1,15 @@
 package com.example.paw_project
 
-fun main(){
+fun nullSafety(){
+    //neverNull has String type
+    var neverNull: String = "This can't be null"
 
+    // Variable nullable of type String? can hold a null value
+    var nullable: String? = "You can keep a null here"
+    // This is OK
+    nullable = null
+}
+
+fun main(){
+    nullSafety()
 }
