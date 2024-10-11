94d868c95766cd95e9415830f16bd7a938316cad
Kakonoomoide <rizkiramada128@gmail.com>
2024-10-11 18:29:16 +0700

    Add - A functions to demonstrate parameter usage: withoutParameter and withParameter

---
+++ app/src/main/java/com/example/paw_project/Functions.kt
@@ -0,0 +1,20 @@
+package com.example.paw_project
+
+// Function withoutParameter
+// - Prints a greeting message without requiring any input parameters.
+fun withoutParameter() {
+    println("== withoutParameter ==")
+    println("Hello, World!")
+}
+
+// Function withParameter
+// - Prints a personalized greeting message.
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
+
+fun main(){
+
+}
