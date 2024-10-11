3c6bcf496a2e1f71862da1edc82de0ae71b30d02
Kakonoomoide <rizkiramada128@gmail.com>
2024-10-11 18:58:09 +0700

    Add - null check for neverNull variable to demonstrate conditional checking

--- app/src/main/java/com/example/paw_project/NullSafety.kt
+++ app/src/main/java/com/example/paw_project/NullSafety.kt
@@ -8,6 +8,14 @@ fun nullSafety(){
     var nullable: String? = "You can keep a null here"
     // This is OK
     nullable = null
+
+    // Check for null in conditions
+    // - This condition checks if the neverNull variable is null.
+    if (neverNull == null) {
+        println("neverNull is null") // This line will never run
+    } else {
+        println("neverNull is not null") // This line will always run
+    }
 }
 
 fun main(){
