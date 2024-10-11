d5f0c2f5a1d8a8133fd2f2e7fe8031072391542c
Kakonoomoide <rizkiramada128@gmail.com>
2024-10-11 18:11:06 +0700

    Add - a data class DataClass to hold user data with id and email properties, and create an instance of DataClass

--- app/src/main/java/com/example/paw_project/DataClass.kt
+++ app/src/main/java/com/example/paw_project/DataClass.kt
@@ -1,2 +1,15 @@
 package com.example.paw_project
 
+// A data class in Kotlin is a special type of class that is primarily used to hold data.
+// Create simple data class with properties: `id` (Int) and `email` (String).
+data class DataClass(
+    val id: Int,
+    var email: String
+)
+
+fun main(){
+    // Create an instance of DataClass
+    val data = DataClass(1, "rizki@gmail.com")
+    println(data)
+
+}
