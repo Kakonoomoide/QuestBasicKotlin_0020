02a16426969f2f3f711bf406c4e30efef50f2600
Kakonoomoide <rizkiramada128@gmail.com>
2024-10-11 16:32:30 +0700

    add - class motor and contact and use them for example oop in kotlin

--- app/src/main/java/com/example/paw_project/Class.kt
+++ app/src/main/java/com/example/paw_project/Class.kt
@@ -1,2 +1,21 @@
 package com.example.paw_project
 
+//create class motor
+class Motor()
+
+//create class Contact
+class Contact(val id:Int, var email:String)
+
+fun main(){
+    //create object for class contact
+    val contact = Contact(1,"rizki@gmail.com")
+
+    //print the one of value object contact
+    println(contact.email)
+
+    //update the value of email in object contact
+    contact.email = "kakonoomoide@gmail.com"
+
+    //print the new email value object contact
+    println(contact.email)
+}
