3d0c53b94e8bef5e4368d2f0341a8237360267bd
Kakonoomoide <rizkiramada128@gmail.com>
2024-10-11 18:45:52 +0700

    add - A function to convert string to uppercase

--- app/src/main/java/com/example/paw_project/LambdaFunction.kt
+++ app/src/main/java/com/example/paw_project/LambdaFunction.kt
@@ -3,6 +3,11 @@ package com.example.paw_project
 // Lambda Functions
 // - They are typically used for creating simple functions.
 
+// Function to convert a string to uppercase
+fun uppercastString(string: String): String {
+    return string.uppercase()
+}
+
 fun main (){
 
 }
